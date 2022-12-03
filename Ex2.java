5de47b2db2d9a2ad0485c76bf79fe7941603aa39
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2022-12-03 15:52:06 +0700

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Ex002 {
+public class Ex2 {
     public static void main(String[] args) {
         List<Integer> numList = new <>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
         List<Integer> evenList = new <>();
